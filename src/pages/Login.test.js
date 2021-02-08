06e48563e04f1398c755043563c488ee2788d7a6
Max Rozen <rozenmd@gmail.com>
2021-02-09 08:55:07 +1100

    refactor for latest best practices in RTL

--- src/pages/Login.test.js
+++ src/pages/Login.test.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import Login from './Login';
-
-test('renders all inputs', () => {
-  const { getByLabelText } = render(<Login />);
-  const userInput = getByLabelText(/username/i);
-  expect(userInput).toBeInTheDocument();
-  const passwordInput = getByLabelText(/password/i);
-  expect(passwordInput).toBeInTheDocument();
-});
 
 test('integration test', async () => {
   const USER = 'some-username';
   const PASS = 'some-pass';
-  const { findByLabelText, findByText } = render(<Login />);
-  const userInput = await findByLabelText(/username/i);
+
+  render(<Login />);
+
+  const userInput = screen.getByLabelText(/username/i);
   user.type(userInput, USER);
-  const passwordInput = await findByLabelText(/password/i);
+  const passwordInput = screen.getByLabelText(/password/i);
   user.type(passwordInput, PASS);
-  const submitButton = await findByText(/submit/i);
+  const submitButton = screen.getByText(/submit/i);
 
   fireEvent.click(submitButton);
-  expect(await findByText(/your username/i)).toBeInTheDocument();
-  expect(await findByText(/your password/i)).toBeInTheDocument();
+  expect(await screen.findByText(/your username/i)).toBeInTheDocument();
+  expect(await screen.findByText(/your password/i)).toBeInTheDocument();
 });
