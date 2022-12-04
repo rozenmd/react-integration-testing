e7781c3b9e86ae84ebc61a651d1980c008e9af29
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-12-04 23:02:48 +0100

    turn it into a test suite, add a basic render test

--- src/pages/Login.test.js
+++ src/pages/Login.test.js
@@ -3,22 +3,32 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Login from "./Login";
 
-test("integration test", async () => {
-  const USER = "some-username";
-  const PASS = "some-pass";
-  const user = userEvent.setup();
+describe("Login form", () => {
+  test("the form loads", () => {
+    render(<Login />);
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/username/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
 
-  render(<Login />);
+  test("happy path", async () => {
+    const USER = "some-username";
+    const PASS = "some-pass";
+    const user = userEvent.setup();
 
-  const userInput = screen.getByLabelText(/username/i);
-  await user.type(userInput, USER);
+    render(<Login />);
 
-  const passwordInput = screen.getByLabelText(/password/i);
-  await user.type(passwordInput, PASS);
+    const userInput = screen.getByLabelText(/username/i);
+    await user.type(userInput, USER);
 
-  const submitButton = screen.getByText(/submit/i);
-  await user.click(submitButton);
+    const passwordInput = screen.getByLabelText(/password/i);
+    await user.type(passwordInput, PASS);
 
-  expect(await screen.findByText(/your username/i)).toBeInTheDocument();
-  expect(await screen.findByText(/your password/i)).toBeInTheDocument();
+    const submitButton = screen.getByText(/submit/i);
+    await user.click(submitButton);
+
+    expect(await screen.findByText(/your username/i)).toBeInTheDocument();
+    expect(await screen.findByText(/your password/i)).toBeInTheDocument();
+  });
 });
